c700fc277d3e2bbd4e85fc724f185b6f2d8c3c30
Neil Gomes <neilgomes327@gmail.com>
2026-07-23 13:26:18 -0400

    Add print-formatted report export to Site Search page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
+import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 
